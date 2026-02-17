6bd7bd0fb0f9ec7633e47615f9e6e953c23248be
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2026-02-17 23:42:28 +0530

    17 February

---
+++ 279_ContainsDuplicate.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     for (int j = i + 1; j < nums.size(); ++j) {
+        //         if (nums[i] == nums[j]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        unordered_set<int> seen;
+
+        for (int n : nums) {
+            if (seen.find(n) != seen.end()) {
+                return true;
+            }
+            seen.insert(n);
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> input = {1, 2, 1};
+    bool result = sol.containsDuplicate(input);
+
+    cout << "Output: " << (result ? "true" : "false") << endl;
+
+    return 0;
+}
+
