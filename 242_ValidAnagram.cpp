852466fb68a62d56c5c823362197f25c860af417
Devansh Sharma <devanshsh25@gmail.com>
2026-02-24 23:09:24 +0530

    February 24: Optimized solution for the valid anagram

--- 242_ValidAnagram.cpp
+++ 242_ValidAnagram.cpp
@@ -6,10 +6,29 @@ using namespace std;
 
 class Solution {
 public:
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    //     return s == t;
+    // }
+
+    // O(n) approach
+
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if (s.length() != t.length()) return false;
+
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int x : count) {
+            if (x != 0) return false;
+        }
+
+        return true;
     }
 };
 
