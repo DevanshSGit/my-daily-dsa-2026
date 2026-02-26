d87c0558ab462f3147de726ad80ee1dcc05856e7
Devansh Sharma <devanshsh25@gmail.com>
2026-02-26 23:58:53 +0530

    February 26: Group Anagrams initial code

---
+++ 49_GroupAnagrams.cpp
@@ -0,0 +1,20 @@
+// Problem 49: Group Anagrams
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+
+    return 0;
+}
+
